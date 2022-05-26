8293726eb2a60c71e6dbf84daedea6e2ede67e4d
32comma <louis.develog@gmail.com>
2022-05-26 13:56:43 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,68 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Socket socket = null;
-		Scanner sc = null;
 		try {
-			socket = new Socket("127.0.0.1", 9090);
+			Socket socket = new Socket("127.0.0.1", 9090);
 			System.out.println("서버에 접속했습니다.");
-			OutputStream os = socket.getOutputStream();
-			PrintWriter pw = new PrintWriter(os);
-			sc = new Scanner(System.in);
-			InputStream is = socket.getInputStream();
-			Scanner sc2 = new Scanner(is);
-			Thread th = new Thread() {
+			Thread listenThread = new Thread() {
 
 				@Override
 				public void run() {
 					try {
-						Thread.sleep(3000);
-						while (sc2.hasNext()) {
-							String message = sc2.nextLine();
-							System.out.println(message);
+						Scanner listenScanner = new Scanner(socket.getInputStream());
+						while (listenScanner.hasNext()) {
+							Thread.sleep(100);
+							String str = listenScanner.nextLine();
+							System.out.println("<<"+str);
+							if (str.equals("exit")) {
+								break;
+							}
 						}
-					} catch (InterruptedException e) {
+						listenScanner.close();
+						socket.close();
+					} catch (Exception e) {
 						e.printStackTrace();
-					} finally {
-						sc2.close();
 					}
 				}
 
 			};
-			th.start();
-			while (true) {
-				String str = sc.nextLine();
-				pw.println(str);
-				pw.flush();
-			}
+			listenThread.start();
+
+			Thread sendThread = new Thread() {
+
+				@Override
+				public void run() {
+					try {
+						Scanner inputScanner = new Scanner(System.in);
+						PrintWriter pw = new PrintWriter(socket.getOutputStream());
+						while (true) {
+							Thread.sleep(100);
+							String str = inputScanner.nextLine();
+							System.out.println(">>"+str);
+							pw.println(str);
+							pw.flush();
+							if (str.equals("exit")) {
+								inputScanner.close();
+								break;
+							}
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				}
+
+			};
+			sendThread.start();
 		} catch (Exception e) {
 			e.printStackTrace();
+
 		} finally {
-			try {
-				socket.close();
-				sc.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+
 		}
+
 	}
 }
